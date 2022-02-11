e5471dbd2406722aade2ebf3d60a31ecaad0af90
onurrbl <70076850+onurrbl@users.noreply.github.com>
2022-02-11 21:58:32 +0300

    style navbar

--- e-commerce-product-page/components/Navbar.js
+++ e-commerce-product-page/components/Navbar.js
@@ -1,21 +1,55 @@
-import React from 'react'
-import Image from 'next/image'
-import logo from './/../images/logo.svg'
-import imgAvatar from './/../images/image-avatar.png'
+import React from "react";
+import Image from "next/image";
+import logo from ".//../images/logo.svg";
+import imgAvatar from ".//../images/image-avatar.png";
+import icnCart from ".//../images/icon-cart.svg";
+import icnMenu from ".//../images/icon-menu.svg";
+import styles from "./Navbar.module.scss";
+
 const Navbar = () => {
   return (
-    <nav className='flex'>
-        <Image src={logo} alt='logo' width={120} height={20} className='al-self-center' ></Image>
-        <ul>
-            <li>Collection</li>
-            <li>Men</li>
-            <li>Women</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-        <Image alt='Avatar' src={imgAvatar} width={50} height={50} className='al-self-center'></Image>
-    </nav>
-  )
-}
+    <nav className={styles.navbar}>
+      <div className="flex al-center justify-center ">
+        <button>
+          {" "}
+          <Image alt="navMenu" src={icnMenu}></Image>
+        </button>
+        <div>
+          <Image
+            src={logo}
+            alt="logo"
+            width={120}
+            height={20}
+            className="al-self-center"
+          ></Image>
+        </div>
+      </div>
 
-export default Navbar
+      <ul >
+        <li>Collection</li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+
+      <div className="flex al-center cart-ctn">
+        <div className={styles.img}>
+          <Image src={icnCart}  alt="cart"></Image>
+        </div>
+
+        <div>
+          <Image
+            alt="Avatar"
+            src={imgAvatar}
+            width={50}
+            height={50}
+            className="al-self-center img"
+          ></Image>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
